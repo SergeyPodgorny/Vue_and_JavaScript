2560f44f790ddc2903d5d9b32bd8c0d647e05b32
SergeyPodgorny <serkonpod@gmail.com>
2023-05-14 22:57:24 +0300

    initial commit

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container">
+
+        <h2 class="h2_text">Login</h2>
+            
+            <div class="input">
+                <input
+                class="form-control"
+                type="username"
+                v-model="username"
+                placeholder="Enter Username"/>
+            </div>
+
+           <div class="input" >
+                <input
+                class="form-control"
+                type="password"
+                v-model="password"
+                placeholder="Enter Password">
+           </div>
+
+           <button type="submit" id="login_button" v-on:click="login">Submit</button>
+    </div>
+</template>
+
+<script>
+
+import AUTHORIZATION_API_URL from '@/constants/authorizationApiUrl.js'
+import axios from 'axios'
+
+export default {
+    name:"LoginForm",
+    data(){
+        return{
+            username:"",
+            password:""
+        }
+    },
+    methods:{
+        login(){
+            const response = axios.post(AUTHORIZATION_API_URL,
+            {
+                username:this.username,
+                password:this.password
+            })
+            console.log(response)            
+        }
+    }
+}
+
+</script>
---
+++ src/constants/authorizationApiUrl.js
@@ -0,0 +1,3 @@
+const AUTHORIZATION_API_URL = 'http://localhost:8080/login/v1/authorization'
+
+export default AUTHORIZATION_API_URL
